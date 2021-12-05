3d16b3ad06ff1d8aa1bea6d0f5d0101628c793f2
Res Kocher <andreas_kocher@bluewin.ch>
2021-12-05 12:24:53 +0100

    prepare exercise 5

--- toDoApp/src/app/app.module.ts
+++ toDoApp/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditCreateTodoComponent } from './todos/edit-create-todo/edit-create-todo.component';
 import { TodoComponent } from './todos/todo/todo.component';
 import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent, TodoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoComponent,
+    EditCreateTodoComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
